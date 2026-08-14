4652ec9280f922f835d04d668a37487d8eda4b0c
XuChao <xuchao@guestin.cn>
2026-08-14 10:30:20 +0800

    fix eof

--- binder.go
+++ binder.go
@@ -1,7 +1,6 @@
 package web
 
 import (
-	"bytes"
 	"io"
 
 	"github.com/labstack/echo/v4"
@@ -14,20 +13,12 @@ type _binder struct {
 func (b *_binder) Bind(i interface{}, c echo.Context) error {
 	req := c.Request()
 	if req.Body != nil && req.ContentLength > 0 {
-		reqBody, _ := io.ReadAll(req.Body)
-		req.Body = io.NopCloser(bytes.NewBuffer(reqBody))
-		defer func() {
-			// reset the body for next bind
-			req.Body = io.NopCloser(bytes.NewBuffer(reqBody))
-		}()
-		//replayBody, ok := req.Body.(io.ReadSeekCloser)
-		//if ok {
-		//	restoreFn, err := mio.SaveSeekerPos(replayBody)
-		//	if err != nil {
-		//		return kerrors.InternalErr(err)
-		//	}
-		//	defer restoreFn()
-		//}
+		replayBody, ok := req.Body.(io.ReadSeekCloser)
+		if ok {
+			defer func() {
+				_, _ = replayBody.Seek(0, io.SeekStart)
+			}()
+		}
 	}
 	return b.under.Bind(i, c)
 }
--- mid/replay.go
+++ mid/replay.go
@@ -1,9 +1,7 @@
 package mid
 
 import (
-	"bytes"
-	"io"
-
+	"github.com/guestin/kboot-web-echo-starter/internal"
 	"github.com/labstack/echo/v4"
 )
 
@@ -12,20 +10,7 @@ func ReqBodyReplay() echo.MiddlewareFunc {
 		return func(c echo.Context) error {
 			req := c.Request()
 			if req.Body != nil && req.ContentLength > 0 {
-				reqBody, _ := io.ReadAll(req.Body)
-				req.Body = io.NopCloser(bytes.NewBuffer(reqBody))
-				defer func() {
-					// reset the body for next bind
-					req.Body = io.NopCloser(bytes.NewBuffer(reqBody))
-				}()
-				//replayBody, ok := req.Body.(io.ReadSeekCloser)
-				//if ok {
-				//	restoreFn, err := mio.SaveSeekerPos(replayBody)
-				//	if err != nil {
-				//		return kerrors.InternalErr(err)
-				//	}
-				//	defer restoreFn()
-				//}
+				c.Request().Body = internal.NewReplayBuffer(c.Request().Body)
 			}
 			return next(c)
 		}
